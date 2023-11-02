c6acb1ea4f229437efde4baa5a10c5ef696bf466
maq-ahmad-20 <Maqboolahmad081@gmail.com>
2023-11-02 17:55:52 +0530

    ExpenseItemCommit

---
+++ src/Components/ExpenseItems.js
@@ -0,0 +1,17 @@
+
+
+function ExpenseItem() {
+    return (
+        <div>
+            <p>Expense Item</p>
+            <ul>
+                <li>Food Rs 100</li>
+                <li>Petrol Rs 100</li>
+                <li>Movies Rs 200</li>
+            </ul>
+        </div>
+
+    );
+}
+
+export default ExpenseItem;
